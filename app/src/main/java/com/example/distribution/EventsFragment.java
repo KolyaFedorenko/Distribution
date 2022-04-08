c98092a0b43a370dd1942e88c6cd1dd5df3d1aa4
Kolya Fedorenko <kolyafedorenko645@gmail.com>
2022-04-09 01:47:40 +0700

    Added getting events from database and displaying them using RecyclerView

--- app/src/main/java/com/example/distribution/EventsFragment.java
+++ app/src/main/java/com/example/distribution/EventsFragment.java
@@ -6,12 +6,22 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class EventsFragment extends Fragment {
 
@@ -21,6 +31,11 @@ public class EventsFragment extends Fragment {
     private EventFragmentInterface eventFragmentInterface;
 
     private Button buttonAddNewEvent;
+    private RecyclerView recyclerViewEvents;
+
+    private DatabaseReference databaseReference;
+    private ArrayList<Event> events;
+    private EventAdapter adapter;
 
     public EventsFragment() { }
 
@@ -38,6 +53,7 @@ public class EventsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        databaseReference = FirebaseDatabase.getInstance().getReference("Events");
     }
 
     @Override
@@ -51,6 +67,15 @@ public class EventsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         buttonAddNewEvent = view.findViewById(R.id.buttonAddNewEvent);
+        recyclerViewEvents = view.findViewById(R.id.recyclerViewEvents);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
+        recyclerViewEvents.setLayoutManager(layoutManager);
+
+        events = new ArrayList<>();
+        adapter = new EventAdapter(getActivity(), events);
+        getEvents();
+        recyclerViewEvents.setAdapter(adapter);
 
         buttonAddNewEvent.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,5 +83,25 @@ public class EventsFragment extends Fragment {
                 eventFragmentInterface.onAddNewEvent();
             }
         });
+    }
+
+    private void getEvents(){
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (events.size() > 0) events.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    Event event = dataSnapshot.getValue(Event.class);
+                    events.add(new Event(event.getEventName(), event.getEventDescription(), event.getEventWorkers(), event.getEventDate()));
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+        databaseReference.addValueEventListener(valueEventListener);
     }
 }
